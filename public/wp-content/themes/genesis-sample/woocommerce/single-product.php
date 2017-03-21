16e7321f24b3367ceffc1f2545fcdfd8fb1d2b0e
Maria <msaav3@gmail.com>
2017-03-21 13:20:04 -0500

    Modified single-product page and styles

--- public/wp-content/themes/genesis-sample/woocommerce/single-product.php
+++ public/wp-content/themes/genesis-sample/woocommerce/single-product.php
@@ -48,6 +48,14 @@ get_header( 'shop' ); ?>
 						<div class="wc-product-description">
 							<?php echo apply_filters( 'woocommerce_short_description', $post->post_excerpt ) ?>
 						</div>
+						<div class="wc-price-box">
+							<span class="price-label highlight">$8,100</span>
+							<button class="wc-custom-btn buy waves-effect waves-light btn"><i class="material-icons right">play_arrow</i>BUY NOW</button>
+						</div>
+						<div class="wc-price-box">
+							<span class="price-label">$1,000</span>
+							<button class="wc-custom-btn deposit waves-effect waves-light btn"><i class="material-icons right">play_arrow</i>DEPOSIT</button>
+						</div>
 					</div>
 				</div>
 				<!-- Thumbnails-->
