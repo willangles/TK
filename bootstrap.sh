9bd0dc580d25caee557f73df21db0fbcda9910bd
Will Angles <willangles@gmail.com>
2017-03-21 12:58:34 -0500

    nav fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,4 @@
-mysqldrump -u root -p"root" -h localhost scotchbox > ../../var/www/database.sql;
+# mysqldump -u root -p"root" -h localhost scotchbox > ../../var/www/database.sql;
+# echo Setting up database..
+# mysql -u root -p"root" -e "DROP DATABASE IF EXISTS scotchbox ; CREATE DATABASE scotchbox;"
+# mysql -u root -p"root" -h localhost scotchbox < ../../var/www/will.sql;
--- public/wp-content/themes/genesis-sample/front-page.php
+++ public/wp-content/themes/genesis-sample/front-page.php
@@ -81,6 +81,23 @@ function my_custom_loop () {
 		</div>
 	</section>
  </div> <!-- end main container -->
+
+
+<!-- THIS WILL CHANGE IN FUTURE AS WE FINALIZE HOW MENU WILL BE SET UP -->
+<script>
+
+jQuery(document).ready(function(){
+	jQuery('#menu-item-87 a').append('<br class="clearfix"><br class="clearfix"><br class="clearfix"><p>Jumbo Super-Pro 59HP Mill with maxed hydraulics</p>');
+	jQuery('#menu-item-110 a').append('<br class="clearfix"><br class="clearfix"><br class="clearfix"><p>XL Super-Pro 49HP Mill with full hydraulics</p>');
+	jQuery('#menu-item-111 a').append('<br class="clearfix"><br class="clearfix"><br class="clearfix"><p>Heavy Industrial 38HP Mill with full hydraulics</p>');
+	jQuery('#menu-item-112 a').append('<br class="clearfix"><br class="clearfix"><br class="clearfix"><p>Massive Mid-size 27HP Mill with basic hydraulics</p>');
+	jQuery('#menu-item-113 a').append('<br class="clearfix"><br class="clearfix"><br class="clearfix"><p>Fully Portable Mill with essential hydraulics</p>');
+	jQuery('#menu-item-114 a').append('<br class="clearfix"><br class="clearfix"><br class="clearfix"><p>World\'s toughest personal mill</p>');
+});
+
+
+</script>
+
 <?php } ?>
 
 
