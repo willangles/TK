fc77464fe0c4526cbeac8d5dd23f01aee62c08b5
Maria <msaav3@gmail.com>
2017-03-28 14:51:06 -0500

    Adjusted product styles and layout

--- public/wp-content/themes/genesis-sample/woocommerce/single-product/product-layout.php
+++ public/wp-content/themes/genesis-sample/woocommerce/single-product/product-layout.php
@@ -5,10 +5,10 @@
     <div class="wc-description-wrapper col s12 m12 l6">
         <!-- Product Title -->
         <?php wc_get_template( 'single-product/title.php' ); ?>
-        <!-- Product Description-->
-        <?php wc_get_template( 'single-product/short-description.php' ); ?>
         <!-- Buy Now and Deposit Buttons -->
         <?php wc_get_template( 'single-product/product-buttons.php' ); ?>
+        <!-- Product Description-->
+        <?php wc_get_template( 'single-product/short-description.php' ); ?>
     </div>
 </div>
 <div class="row">
