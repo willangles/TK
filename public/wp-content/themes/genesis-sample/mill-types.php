48e6583dc59c14a3881bfc089334646f3c8e1f7b
Will Angles <willangles@gmail.com>
2017-03-31 11:11:38 -0500

    Mill Type Styling and finish

--- public/wp-content/themes/genesis-sample/mill-types.php
+++ public/wp-content/themes/genesis-sample/mill-types.php
@@ -10,68 +10,74 @@ function my_custom_loop () {
 ?>
 
 <section id="mill-types-header" class="valign-wrapper" style="background-image:url(<?php the_post_thumbnail_url(); ?>)">
-    <h3 class="valign"><?php single_post_title(); ?></h3>
+    <h2 class="valign"><?php single_post_title(); ?></h2>
 </section>
 
 <section id="mill-focus">
     <article id="focus-intro" class="row focus-item">
         <div class="col s12">
-            <p>1Lorem ipsum dolor sit amet, an duo dicta nihil interpretaris, ut quo nihil definitionem. Pri an facete reprehendunt, ei tollit iudicabit nam, no corpora efficiantur nam</p>
-            <p><a class="btn btn-default">Learn More</a></p>
+            <h3><?php the_field('focus_intro_headline'); ?></h3>
+            <p><?php the_field('focus_intro_content'); ?></p>
         </div>
     </article>
     <article id="focus-1220" class="row hidden focus-item">
         <div class="col m6 s12">
-            <img src="http://www.norwoodsawmills.com/sites/norwood/files/lumberprohd36.png">
+            <img src="<?php the_field('focus1220_image'); ?>">
         </div>
         <div class="col m6 s12">
-            <p>2Lorem ipsum dolor sit amet, an duo dicta nihil interpretaris, ut quo nihil definitionem. Pri an facete reprehendunt, ei tollit iudicabit nam, no corpora efficiantur nam</p>
-            <p><a class="btn btn-default">Learn More</a></p>
+            <h3><?php the_field('focus1220_headline'); ?></h3>
+            <p><?php the_field('focus1220_content'); ?></p>
+            <p style="text-align: center;"><a class="btn btn-default" href="<?php the_field('focus1220_link'); ?>">Learn More</a></p>
         </div>
     </article>
     <article id="focus-1400" class="row hidden focus-item">
         <div class="col m6 s12">
-            <img src="http://www.norwoodsawmills.com/sites/norwood/files/lumberprohd36.png">
+            <img src="<?php the_field('focus1400_image'); ?>">
         </div>
         <div class="col m6 s12">
-            <p>3Lorem ipsum dolor sit amet, an duo dicta nihil interpretaris, ut quo nihil definitionem. Pri an facete reprehendunt, ei tollit iudicabit nam, no corpora efficiantur nam</p>
-            <p><a class="btn btn-default">Learn More</a></p>
+            <h3><?php the_field('focus1400_headline'); ?></h3>
+            <p><?php the_field('focus1400_content'); ?></p>
+            <p style="text-align: center;"><a class="btn btn-default" href="<?php the_field('focus1400_link'); ?>">Learn More</a></p>
         </div>
     </article>
     <article id="focus-1600" class="row hidden focus-item">
         <div class="col m6 s12">
-            <img src="http://www.norwoodsawmills.com/sites/norwood/files/lumberprohd36.png">
+            <img src="<?php the_field('focus1600_image'); ?>">
         </div>
         <div class="col m6 s12">
-            <p>1Lorem ipsum dolor sit amet, an duo dicta nihil interpretaris, ut quo nihil definitionem. Pri an facete reprehendunt, ei tollit iudicabit nam, no corpora efficiantur nam</p>
-            <p><a class="btn btn-default">Learn More</a></p>
+            <h3><?php the_field('focus1600_headline'); ?></h3>
+            <p><?php the_field('focus1600_content'); ?></p>
+            <p style="text-align: center;"><a class="btn btn-default" href="<?php the_field('focus1600_link'); ?>">Learn More</a></p>
         </div>
     </article>
     <article id="focus-2000" class="row hidden focus-item">
         <div class="col m6 s12">
-            <img src="http://www.norwoodsawmills.com/sites/norwood/files/lumberprohd36.png">
+            <img src="<?php the_field('focus2000_image'); ?>">
         </div>
         <div class="col m6 s12">
-            <p>1Lorem ipsum dolor sit amet, an duo dicta nihil interpretaris, ut quo nihil definitionem. Pri an facete reprehendunt, ei tollit iudicabit nam, no corpora efficiantur nam</p>
-            <p><a class="btn btn-default">Learn More</a></p>
+            <h3><?php the_field('focus2000_headline'); ?></h3>
+            <p><?php the_field('focus2000_content'); ?></p>
+            <p style="text-align: center;"><a class="btn btn-default" href="<?php the_field('focus2000_link'); ?>">Learn More</a></p>
         </div>
     </article>
     <article id="focus-2200" class="row hidden focus-item">
         <div class="col m6 s12">
-            <img src="http://www.norwoodsawmills.com/sites/norwood/files/lumberprohd36.png">
+            <img src="<?php the_field('focus2200_image'); ?>">
         </div>
         <div class="col m6 s12">
-            <p>1Lorem ipsum dolor sit amet, an duo dicta nihil interpretaris, ut quo nihil definitionem. Pri an facete reprehendunt, ei tollit iudicabit nam, no corpora efficiantur nam</p>
-            <p><a class="btn btn-default">Learn More</a></p>
+            <h3><?php the_field('focus2200_headline'); ?></h3>
+            <p><?php the_field('focus2200_content'); ?></p>
+            <p style="text-align: center;"><a class="btn btn-default" href="<?php the_field('focus2200_link'); ?>">Learn More</a></p>
         </div>
     </article>
     <article id="focus-2500" class="row hidden focus-item">
         <div class="col m6 s12">
-            <img src="http://www.norwoodsawmills.com/sites/norwood/files/lumberprohd36.png">
+            <img src="<?php the_field('focus2500_image'); ?>">
         </div>
         <div class="col m6 s12">
-            <p>1Lorem ipsum dolor sit amet, an duo dicta nihil interpretaris, ut quo nihil definitionem. Pri an facete reprehendunt, ei tollit iudicabit nam, no corpora efficiantur nam</p>
-            <p><a class="btn btn-default">Learn More</a></p>
+            <h3><?php the_field('focus2500_headline'); ?></h3>
+            <p><?php the_field('focus2500_content'); ?></p>
+            <p style="text-align: center;"><a class="btn btn-default" href="<?php the_field('focus2500_link'); ?>">Learn More</a></p>
         </div>
     </article>
 </section>
@@ -80,63 +86,63 @@ function my_custom_loop () {
   	<div class="row">
       <div class="col m4 s6 ">
           <div class="row">
-              <div class="col m6 s12">
-                  <a class="mill-type" id="type-1220"><img src="http://www.norwoodsawmills.com/sites/norwood/files/lumberprohd36.png"></a>
+              <div class="col s12">
+                  <div class="mill-type type-1220"><img src="<?php the_field('focus1220_image'); ?>"></div>
               </div>
-              <div class="col m6 s12">
-                  <p>Lorem ipsum dolor sit amet, an duo dicta nihil interpretaris, ut quo nihil definitionem. Pri an facete reprehendunt, ei tollit iudicabit nam, no corpora efficiantur nam. <a class="mill-type" id="type-1220">Learn More</a></p>
-              </div>
-          </div>
-      </div>
-      <div class="col m4 s6 ">
-          <div class="row">
-              <div class="col m6 s12">
-                  <a class="mill-type" id="type-1400"><img src="http://www.norwoodsawmills.com/sites/norwood/files/lumberprohd36.png"></a>
-              </div>
-              <div class="col m6 s12">
-                  <p>Lorem ipsum dolor sit amet, an duo dicta nihil interpretaris, ut quo nihil definitionem. Pri an facete reprehendunt, ei tollit iudicabit nam, no corpora efficiantur nam. <a class="mill-type" id="type-1400">Learn More</a></p>
-              </div>
-          </div>
-      </div>
-
-      <div class="col m4 s6 ">
-          <div class="row">
-              <div class="col m6 s12">
-                  <a class="mill-type" id="type-1600"><img src="http://www.norwoodsawmills.com/sites/norwood/files/lumberprohd36.png"></a>
-              </div>
-              <div class="col m6 s12">
-                  <p>Lorem ipsum dolor sit amet, an duo dicta nihil interpretaris, ut quo nihil definitionem. Pri an facete reprehendunt, ei tollit iudicabit nam, no corpora efficiantur nam. <a class="mill-type" id="type-1600">Learn More</a></p>
+              <div class="col s12">
+                  <h3 class="mill-type type-1400"><?php the_field('focus1220_headline'); ?></h3>
               </div>
           </div>
       </div>
       <div class="col m4 s6 ">
           <div class="row">
-              <div class="col m6 s12">
-                  <a class="mill-type" id="type-2000"><img src="http://www.norwoodsawmills.com/sites/norwood/files/lumberprohd36.png"></a>
+              <div class="col s12">
+                  <div class="mill-type type-1400"><img src="<?php the_field('focus1400_image'); ?>"></div>
               </div>
-              <div class="col m6 s12">
-                  <p>Lorem ipsum dolor sit amet, an duo dicta nihil interpretaris, ut quo nihil definitionem. Pri an facete reprehendunt, ei tollit iudicabit nam, no corpora efficiantur nam. <a class="mill-type" id="type-2000">Learn More</a></p>
+              <div class="col s12">
+                  <h3 class="mill-type type-1400"><?php the_field('focus1400_headline'); ?></h3>
               </div>
           </div>
       </div>
 
       <div class="col m4 s6 ">
           <div class="row">
-              <div class="col m6 s12">
-                  <a class="mill-type" id="type-2200"><img src="http://www.norwoodsawmills.com/sites/norwood/files/lumberprohd36.png"></a>
+              <div class="col s12">
+                  <div class="mill-type type-1600"><img src="<?php the_field('focus1600_image'); ?>"></div>
               </div>
-              <div class="col m6 s12">
-                  <p>Lorem ipsum dolor sit amet, an duo dicta nihil interpretaris, ut quo nihil definitionem. Pri an facete reprehendunt, ei tollit iudicabit nam, no corpora efficiantur nam. <a class="mill-type" id="type-2200">Learn More</a></p>
+              <div class="col s12">
+                  <h3 class="mill-type type-1400"><?php the_field('focus1600_headline'); ?></h3>
               </div>
           </div>
       </div>
       <div class="col m4 s6 ">
           <div class="row">
-              <div class="col m6 s12">
-                  <a class="mill-type" id="type-2500"><img src="http://www.norwoodsawmills.com/sites/norwood/files/lumberprohd36.png"></a>
+              <div class="col s12">
+                  <div class="mill-type type-2000"><img src="<?php the_field('focus2000_image'); ?>"></div>
               </div>
-              <div class="col m6 s12">
-                  <p>Lorem ipsum dolor sit amet, an duo dicta nihil interpretaris, ut quo nihil definitionem. Pri an facete reprehendunt, ei tollit iudicabit nam, no corpora efficiantur nam. <a class="mill-type" id="type-2500">Learn More</a></p>
+              <div class="col s12">
+                  <h3 class="mill-type type-1400"><?php the_field('focus2000_headline'); ?></h3>
+              </div>
+          </div>
+      </div>
+
+      <div class="col m4 s6 ">
+          <div class="row">
+              <div class="col s12">
+                  <div class="mill-type type-2200"><img src="<?php the_field('focus2200_image'); ?>"></div>
+              </div>
+              <div class="col s12">
+                  <h3 class="mill-type type-1400"><?php the_field('focus2200_headline'); ?></h3>
+              </div>
+          </div>
+      </div>
+      <div class="col m4 s6 ">
+          <div class="row">
+              <div class="col s12">
+                  <div class="mill-type type-2500"><img src="<?php the_field('focus2500_image'); ?>"></div>
+              </div>
+              <div class="col s12">
+                  <h3 class="mill-type type-1400"><?php the_field('focus2500_headline'); ?></h3>
               </div>
           </div>
       </div>
@@ -155,40 +161,46 @@ function my_custom_loop () {
         f2200 = jQuery('#focus-2200'),
         f2500 = jQuery('#focus-2500'),
         fintro = jQuery('#focus-intro'),
-        t1220 = jQuery('#type-1220'),
-        t1400 = jQuery('#type-1400'),
-        t1600 = jQuery('#type-1600'),
-        t2000 = jQuery('#type-2000'),
-        t2200 = jQuery('#type-2200'),
-        t2500 = jQuery('#type-2500');
+        t1220 = jQuery('.type-1220'),
+        t1400 = jQuery('.type-1400'),
+        t1600 = jQuery('.type-1600'),
+        t2000 = jQuery('.type-2000'),
+        t2200 = jQuery('.type-2200'),
+        t2500 = jQuery('.type-2500');
 
     t1220.click(function() {
         focus.hide(200);
+        jQuery('html,body').animate({scrollTop:500},400);
         f1220.show(400);
         console.log('1220');
     });
     t1400.click(function() {
         focus.hide(200);
+        jQuery('html,body').animate({scrollTop:500},400);
         f1400.show(400);
         console.log('1400');
     });
     t1600.click(function() {
         focus.hide(200);
+        jQuery('html,body').animate({scrollTop:500},400);
         f1600.show(400);
         console.log('1600');
     });
     t2000.click(function() {
         focus.hide(200);
+        jQuery('html,body').animate({scrollTop:500},400);
         f2000.show(400);
         console.log('2000');
     });
     t2200.click(function() {
         focus.hide(200);
+        jQuery('html,body').animate({scrollTop:500},400);
         f2200.show(400);
         console.log('2200');
     });
     t2500.click(function() {
         focus.hide(200);
+        jQuery('html,body').animate({scrollTop:500},400);
         f2500.show(400);
         console.log('2500');
     });
