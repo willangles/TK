eb08b89c5a9c824b540cb32c6638afb05a8197d6
Maria <msaav3@gmail.com>
2017-04-05 10:52:20 -0500

    Styled..

--- public/wp-content/themes/genesis-sample/mill-types.php
+++ public/wp-content/themes/genesis-sample/mill-types.php
@@ -21,7 +21,7 @@ function my_custom_loop () {
         </div>
     </article>
 
-    <article id="focus-1220" class="row hidden focus-item" style="background: url(<?php the_field('focus1220_image'); ?>); background-size: cover; background-position: center;">
+    <article id="focus-1220" class="row hidden focus-item product-focus-item" style="background: url(<?php the_field('focus1220_image'); ?>); background-size: cover; background-position: center;">
         <div class="col m6 s12"></div>
         <div class="col m6 s12">
             <h3><?php the_field('focus1220_headline'); ?></h3>
@@ -30,7 +30,7 @@ function my_custom_loop () {
         </div>
     </article>
     
-    <article id="focus-1400" class="row hidden focus-item" style="background: url(<?php the_field('focus1400_image'); ?>); background-size: cover; background-position: center;">
+    <article id="focus-1400" class="row hidden focus-item product-focus-item" style="background: url(<?php the_field('focus1400_image'); ?>); background-size: cover; background-position: center;">
         <div class="col m6 s12"></div>
         <div class="col m6 s12">
             <h3><?php the_field('focus1400_headline'); ?></h3>
@@ -38,7 +38,7 @@ function my_custom_loop () {
             <p style="text-align: center;"><a class="btn btn-default" href="<?php the_field('focus1400_link'); ?>">Learn More</a></p>
         </div>
     </article>
-    <article id="focus-1600" class="row hidden focus-item" style="background: url(<?php the_field('focus1600_image'); ?>); background-size: cover; background-position: center;">
+    <article id="focus-1600" class="row hidden focus-item product-focus-item" style="background: url(<?php the_field('focus1600_image'); ?>); background-size: cover; background-position: center;">
         <div class="col m6 s12"></div>
         <div class="col m6 s12">
             <h3><?php the_field('focus1600_headline'); ?></h3>
@@ -46,7 +46,7 @@ function my_custom_loop () {
             <p style="text-align: center;"><a class="btn btn-default" href="<?php the_field('focus1600_link'); ?>">Learn More</a></p>
         </div>
     </article>
-    <article id="focus-2000" class="row hidden focus-item" style="background: url(<?php the_field('focus2000_image'); ?>); background-size: cover; background-position: center;">
+    <article id="focus-2000" class="row hidden focus-item product-focus-item" style="background: url(<?php the_field('focus2000_image'); ?>); background-size: cover; background-position: center;">
         <div class="col m6 s12"></div>
         <div class="col m6 s12">
             <h3><?php the_field('focus2000_headline'); ?></h3>
@@ -54,7 +54,7 @@ function my_custom_loop () {
             <p style="text-align: center;"><a class="btn btn-default" href="<?php the_field('focus2000_link'); ?>">Learn More</a></p>
         </div>
     </article>
-    <article id="focus-2200" class="row hidden focus-item" style="background: url(<?php the_field('focus2200_image'); ?>); background-size: cover; background-position: center;">
+    <article id="focus-2200" class="row hidden focus-item product-focus-item" style="background: url(<?php the_field('focus2200_image'); ?>); background-size: cover; background-position: center;">
         <div class="col m6 s12"></div>
         <div class="col m6 s12">
             <h3><?php the_field('focus2200_headline'); ?></h3>
@@ -62,7 +62,7 @@ function my_custom_loop () {
             <p style="text-align: center;"><a class="btn btn-default" href="<?php the_field('focus2200_link'); ?>">Learn More</a></p>
         </div>
     </article>
-    <article id="focus-2500" class="row hidden focus-item" style="background: url(<?php the_field('focus2500_image'); ?>); background-size: cover; background-position: center;">
+    <article id="focus-2500" class="row hidden focus-item product-focus-item" style="background: url(<?php the_field('focus2500_image'); ?>); background-size: cover; background-position: center;">
         <div class="col m6 s12"></div>
         <div class="col m6 s12">
             <h3><?php the_field('focus2500_headline'); ?></h3>
