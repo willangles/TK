3eab33fcb6ff23e0e5a31e7e4c4646cad3b095f9
Will Angles <willangles@gmail.com>
2017-04-05 21:04:26 -0500

    product info qa hidden

--- public/wp-content/themes/genesis-sample/woocommerce/single-product/product-info-nav.php
+++ public/wp-content/themes/genesis-sample/woocommerce/single-product/product-info-nav.php
@@ -3,6 +3,6 @@
         <li class="info-panel"><a id="tk-product-features">Features</a></li>
         <li class="info-panel"><a id="tk-product-specs">Specifications</a></li>
         <li class="info-panel"><a id="tk-product-warranty">Warranty</a></li>
-        <li class="info-panel"><a id="tk-product-qa">Q&amp;A</a></li>
+        <!-- <li class="info-panel"><a id="tk-product-qa">Q&amp;A</a></li> -->
     </ul>
 </div>
