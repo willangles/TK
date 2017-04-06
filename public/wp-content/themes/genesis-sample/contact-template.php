47bd8fbecafbad322cc717f0629ac2e30c2e70be
Maria <msaav3@gmail.com>
2017-04-05 20:15:32 -0500

    Adding form styles..

---
+++ public/wp-content/themes/genesis-sample/contact-template.php
@@ -0,0 +1,36 @@
+<?php
+/**
+* Template Name: Form Template
+* Description: Form template
+* 
+* 
+*/
+// Add our custom loop
+remove_action( 'genesis_loop', 'genesis_do_loop' );
+add_action( 'genesis_loop', 'form_template' );
+
+
+function form_template() { ?>
+	<div class="form-template-container">
+		<div class="row">
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post();?>
+			<div class="col s12 m6">
+				<h1 class="alt-title"><?php the_title(); ?></h1>
+				<div class="alt-content">
+					<p><?php the_content(); ?></p>
+				</div>
+			</div>
+			<?php endwhile; endif; ?>
+			<div class="col s12 m6 form-wrapper">
+				<?php $post_object = get_field('contact_form');
+					if( $post_object ): $post = $post_object; setup_postdata( $post ); 
+						$post_id = get_the_ID($post_object->ID);
+						$post_title = get_the_title($post_object->ID);
+						echo do_shortcode('[contact-form-7 id="' . $post_id .'" title="' . $post_title . '"]'); 
+						wp_reset_postdata(); endif; ?>
+			</div>
+		</div>
+	</div>
+<?php }
+
+genesis();
