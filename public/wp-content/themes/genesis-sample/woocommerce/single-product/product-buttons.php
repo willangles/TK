3970a1d09bf88a104a1fbf8a5c6239855fc07e8c
Maria <msaav3@gmail.com>
2017-04-06 16:27:18 -0500

    Different buttons display for product type

--- public/wp-content/themes/genesis-sample/woocommerce/single-product/product-buttons.php
+++ public/wp-content/themes/genesis-sample/woocommerce/single-product/product-buttons.php
@@ -5,10 +5,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 global $product; ?>
 
 <div class="wc-price-box">
-    <span class="price-label highlight"><?php echo $product->get_price_html(); ?></span>
-    <a href="/get-quote" class="wc-custom-btn buy waves-effect waves-light btn">Get Detailed Quote</a>
-    <a href="/get-financed" class="wc-custom-btn accent-btn buy waves-effect waves-light btn">Get Financed</a>
-    <a onclick="addToCart(event);" data-type="<?php the_field('product_type'); ?>" data-id="<?php echo $product->id ?>" class="wc-custom-btn buy accent-btn waves-effect waves-light btn"><i class="material-icons right">play_arrow</i>BUY NOW</a>
+    <?php $type = get_field('product_type'); if($type == 'Product'){ ?>
+        <span class="price-label highlight"><?php echo $product->get_price_html(); ?></span>
+        <a href="/get-quote" class="wc-custom-btn buy waves-effect waves-light btn">Get Detailed Quote</a>
+        <a href="/get-financed" class="wc-custom-btn accent-btn buy waves-effect waves-light btn">Get Financed</a>
+        <a onclick="addToCart(event);" data-type="<?php the_field('product_type'); ?>" data-id="<?php echo $product->id ?>" class="wc-custom-btn buy accent-btn waves-effect waves-light btn"><i class="material-icons right">play_arrow</i>BUY NOW</a>
+    <?php } else { ?>
+        <span class="price-label highlight"><?php echo $product->get_price_html(); ?></span>
+        <a onclick="addToCart(event);" data-type="<?php the_field('product_type'); ?>" data-id="<?php echo $product->id ?>" class="wc-custom-btn buy waves-effect waves-light btn"><i class="material-icons right">play_arrow</i>BUY NOW</a>
+    <?php } ?>
 </div>
 
 <script>
