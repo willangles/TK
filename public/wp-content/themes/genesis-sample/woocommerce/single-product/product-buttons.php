d354258ed00aabacb5eb425d23e61d6df8634b3f
Maria <msaav3@gmail.com>
2017-04-06 16:13:52 -0500

    Styled buttons!

--- public/wp-content/themes/genesis-sample/woocommerce/single-product/product-buttons.php
+++ public/wp-content/themes/genesis-sample/woocommerce/single-product/product-buttons.php
@@ -7,8 +7,8 @@ global $product; ?>
 <div class="wc-price-box">
     <span class="price-label highlight"><?php echo $product->get_price_html(); ?></span>
     <a href="/get-quote" class="wc-custom-btn buy waves-effect waves-light btn">Get Detailed Quote</a>
-    <a href="/get-financed" class="wc-custom-btn buy waves-effect waves-light btn">Get Financed</a>
-    <a onclick="addToCart(event);" data-type="<?php the_field('product_type'); ?>" data-id="<?php echo $product->id ?>" class="wc-custom-btn buy waves-effect waves-light btn"><i class="material-icons right">play_arrow</i>BUY NOW</a>
+    <a href="/get-financed" class="wc-custom-btn accent-btn buy waves-effect waves-light btn">Get Financed</a>
+    <a onclick="addToCart(event);" data-type="<?php the_field('product_type'); ?>" data-id="<?php echo $product->id ?>" class="wc-custom-btn buy accent-btn waves-effect waves-light btn"><i class="material-icons right">play_arrow</i>BUY NOW</a>
     
    
  
