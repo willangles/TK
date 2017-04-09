5c17786d7ab4cb0c49743e477e5cf041ab1f7379
Maria <msaav3@gmail.com>
2017-04-09 16:39:11 -0500

    promo button removed

--- public/wp-content/themes/genesis-sample/front-page.php
+++ public/wp-content/themes/genesis-sample/front-page.php
@@ -102,7 +102,6 @@ function my_custom_loop () {
 				<div class="valign">
 					<h2><?php the_field('promo_title'); ?></h2>
 					<p><?php the_field('promo_copy'); ?></p>
-					<a style="max-width: 250px;" href="<?php the_field('promo_button_link')?>" class="waves-effect waves-light btn button"><?php the_field('promo_button_copy'); ?><i class="material-icons right">play_arrow</i></a>
 					<p></p>
 				</div>
 			</div>
