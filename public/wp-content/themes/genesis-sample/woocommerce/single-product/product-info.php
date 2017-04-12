3f84bc19e567c0e8f3a87165f1a62f6148d23903
Will Angles <willangles@gmail.com>
2017-04-12 17:27:21 -0500

    moveable guide roller

--- public/wp-content/themes/genesis-sample/woocommerce/single-product/product-info.php
+++ public/wp-content/themes/genesis-sample/woocommerce/single-product/product-info.php
@@ -24,7 +24,7 @@
 					<p class="spec-item"><strong>Log Deck Cross Beams</strong><span><?php the_field('log_deck_cross_beams'); ?></span></p>
 					<p class="spec-item"><strong>Blade Clutch</strong><span><?php the_field('blade_clutch'); ?></span></p>
 					<p class="spec-item"><strong>Control Height from Ground</strong><span><?php the_field('control_height'); ?></span></p>
-					<p class="spec-item"><strong>Moveable Guide Roller</strong><span><?php the_field('moveable_guide_roller'); ?></span></p>
+					<p class="spec-item"><strong>Moveable Guide Roller</strong><span><?php the_field('moveable_guide'); ?></span></p>
 					<p class="spec-item"><strong>Command Post</strong><span><?php the_field('command_post'); ?></span></p>
 					<p class="spec-item hide-1220"><strong>Direct-Action Hydraulic System</strong><span><?php the_field('direct-action_hydraulics'); ?></span></p>
 					<p class="spec-item"><strong>Feed System</strong><span><?php the_field('feed_system'); ?></span></p>
